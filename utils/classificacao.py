15e131feed96899f544d947e9bb7b859c97ea590
sportech338 <sportechofc01@gmail.com>
2026-01-06 22:11:01 -0300

    Update classificacao.py

--- utils/classificacao.py
+++ utils/classificacao.py
@@ -169,7 +169,7 @@ def agregar_por_cliente(df_pedidos: pd.DataFrame) -> pd.DataFrame:
     # 7. RESETAR INDEX
     # ======================================================
     df_clientes = df_clientes.reset_index(drop=True)
-    AC    
+        
     return df_clientes
 
 
